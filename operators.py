2e626836930929a634cbdcced43645f0e9d7f675
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-13 22:24:38 +0530

    Add operators.py for basic arithmetic and logical operations

---
+++ operators.py
@@ -0,0 +1,21 @@
+a = 10
+b = 7
+print(a,"+",b,"=",(a+b))
+print(a,"-",b,"=",(a-b))
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+print(a,">",b,"--",(a>b))
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(x,"not",y,"--",(not y))
