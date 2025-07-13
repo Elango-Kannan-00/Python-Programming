98d1a471d4373d739766c526aaae2a7a82b1cf9b
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-13 22:23:46 +0530

    Add read and display.py for variable input and type printing

---
+++ read and display.py
@@ -0,0 +1,11 @@
+a = int(input("Enter any number: "))
+print("The value is: ", a)
+print(type(a))
+
+b = float(input("Enter any number: "))
+print("The value is: ", b)
+print(type(b))
+
+c = input("Enter any number: ")
+print("The value is: ", c)
+print(type(c))
