cc61dabdb4384612d59259454e199b32f024008b
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-15 23:12:44 +0530

    Add addition.py for basic arithmetic operations with user input

---
+++ addition.py
@@ -0,0 +1,25 @@
+a = int(input("Enter the first value: "))
+b = int(input("Enter the second value: "))
+c = a + b 
+print("The sum of the given two numbers is: ", c)
+
+a = float(input("Enter the first value: "))
+b = float(input("Enter the second value: "))
+c = a + b 
+print("The sum of the given two numbers is: ", c)
+
+a = input("Enter the first value: ")
+b = input("Enter the second value: ")
+c = a + b 
+print("The sum of the given two numbers is: ", c)
+
+# output
+# Enter the first value: 5
+# Enter the second value: 3
+# The sum of the given two numbers is:  8
+# Enter the first value: 8.9
+# Enter the second value: 55.6
+# The sum of the given two numbers is:  64.5
+# Enter the first value: a
+# Enter the second value: f
+# The sum of the given two numbers is:  af
