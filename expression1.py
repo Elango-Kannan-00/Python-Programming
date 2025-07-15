495dbbcc1f0c86cd57ef0eacd735bfd7b8dae361
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-15 23:16:24 +0530

    Add expression1.py for arithmetic expression evaluation

---
+++ expression1.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = float(input())
+d = float(input())
+print("(",a,"+",b,")" ,"*", "(",c,"-",d,")","is:", (a+b)*(c-d))
+
+# output
+# 5
+# 6
+# 2.3
+# 5.6
+# ( 5 + 6 ) * ( 2.3 - 5.6 ) is: -36.3
