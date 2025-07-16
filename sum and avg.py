a7b14e3c5c3e2be1ac667b097b4ce036468521ca
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-16 23:12:21 +0530

    Add sum and avg.py for total and average marks calculation

---
+++ sum and avg.py
@@ -0,0 +1,9 @@
+a = int(input("Enter Mark1: "))
+b = int(input("Enter Mark2: "))
+c = int(input("Enter Mark3: "))
+d = int(input("Enter Mark4: "))
+e = int(input("Enter Mark5: "))
+total = a+b+c+d+e 
+avg = total/5
+print("The total marks are:", total) 
+print("The average is:" ,avg)
