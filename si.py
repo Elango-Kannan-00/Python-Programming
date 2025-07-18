b213f53f7268fe892c5692116ce7bded29d5ffc2
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-18 22:43:39 +0530

    add simple interest file for basic understanding.

---
+++ si.py
@@ -0,0 +1,5 @@
+p = int(input("Enter the principal amount: "))
+r = float(input("Enter the rate of interest: "))
+t = float(input("Enter the time duration: "))
+simpInterest = p*r*t/100
+print("The SI for your Principal amount is: ", simpInterest)
