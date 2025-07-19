12d27e343da9000a443e31764c4b283985bdee12
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-19 23:13:49 +0530

    add output for sum and avg.py

--- sum and avg.py
+++ sum and avg.py
@@ -7,3 +7,12 @@ total = a+b+c+d+e
 avg = total/5
 print("The total marks are:", total) 
 print("The average is:" ,avg)
+
+# output
+# Enter Mark1: 50
+# Enter Mark2: 48
+# Enter Mark3: 79
+# Enter Mark4: 100
+# Enter Mark5: 98
+# The total marks are: 375
+# The average is: 75.0
