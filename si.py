fb7084d75a65db14b22f779d7ace2217e223ebeb
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-19 14:59:50 +0530

    add output for si.py

--- si.py
+++ si.py
@@ -3,3 +3,9 @@ r = float(input("Enter the rate of interest: "))
 t = float(input("Enter the time duration: "))
 simpInterest = p*r*t/100
 print("The SI for your Principal amount is: ", simpInterest)
+
+# output
+# Enter the principal amount: 100000
+# Enter the rate of interest: 1.5
+# Enter the time duration: 1
+# The SI for your Principal amount is:  1500.0
