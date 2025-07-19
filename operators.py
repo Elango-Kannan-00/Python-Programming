07476f80d923a91b24dd8c4197ba23355feff5a4
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-19 23:16:41 +0530

    add output for operators.py

--- operators.py
+++ operators.py
@@ -19,3 +19,20 @@ y = False
 print(x and y)
 print(x or y)
 print(x,"not",y,"--",(not y))
+
+# output
+# 10 + 7 = 17
+# 10 - 7 = 3
+# 1.4285714285714286
+# 3
+# 1
+# 10000000
+# 10 > 7 -- True
+# False
+# True
+# False
+# False
+# True
+# False
+# True
+# True not False -- True
