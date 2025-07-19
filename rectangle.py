004d864605de06b552c5f8ce21c0dff5cbc3d076
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-19 23:15:26 +0530

    add output for rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -2,3 +2,8 @@ l = float(input("Enter the length of the Rectangle: "))
 b = float(input("Enter the breadth of the Rectangle: "))
 area = l*b
 print("The area of Rectangle is:", area, "sq.mtr")
+
+# output
+# Enter the length of the Rectangle: 5
+# Enter the breadth of the Rectangle: 6
+# The area of Rectangle is: 30.0 sq.mtr
