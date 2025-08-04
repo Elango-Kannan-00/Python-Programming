0523b87b92e0830096df048069236a5becf33756
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-08-04 23:59:32 +0530

    add mint to seconds conversion script

---
+++ mintToseconds.py
@@ -0,0 +1,12 @@
+hr = int(input("Enter Hour: "))
+mint = int(input("Enter Minutes: "))
+sec = int(input("Enter Seconds: "))
+a = hr*3600
+b = mint*60 
+print("The total seconds in given time span is: ",(a+b+sec))
+
+# output
+# Enter Hour: 2
+# Enter Minutes: 30
+# Enter Seconds: 15
+# The total seconds in given time span is:  9015
